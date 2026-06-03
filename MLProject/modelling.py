acacce42dda0335c0814e6d2ad0fc9a77b3e006e
jameslay1907 <jameslayjames@yahoo.com>
2026-06-03 20:52:42 +0700

    Update dataset path

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -4,6 +4,7 @@ from xgboost import XGBClassifier
 import numpy as np
 import sys
 import warnings
+from pathlib import Path
 
 if __name__ == "__main__":
     warnings.filterwarnings("ignore")
@@ -11,8 +12,10 @@ if __name__ == "__main__":
 
     datasetPath = sys.argv[4] if len(sys.argv) > 4 else 'ai_student_impact_preprocessing'
 
+    BASE_DIR = Path(__file__).resolve().parent
+
     def returndataset(datasetname):
-        dataset = pd.read_csv(f'MLProject/{datasetPath}/{datasetname}.csv')
+        dataset = pd.read_csv(f'{BASE_DIR}/{datasetPath}/{datasetname}.csv')
         dataset = dataset.to_numpy()
         return dataset
 
@@ -26,23 +29,23 @@ if __name__ == "__main__":
     max_depth = int(sys.argv[2]) if len(sys.argv) > 2 else 6
     learning_rate = float(sys.argv[3]) if len(sys.argv) > 3 else 0.05
 
-    with mlflow.start_run():
-        model = XGBClassifier(
-                objective='multi:softprob',
-                num_class=3,
-                n_estimators=n_estimators,
-                learning_rate=learning_rate,
-                max_depth=max_depth,
-                random_state=42
-        )
+    # with mlflow.start_run():
+    #     model = XGBClassifier(
+    #             objective='multi:softprob',
+    #             num_class=3,
+    #             n_estimators=n_estimators,
+    #             learning_rate=learning_rate,
+    #             max_depth=max_depth,
+    #             random_state=42
+    #     )
 
-        model.fit(X_train, y_train)
+    #     model.fit(X_train, y_train)
 
-        predicted_data = model.predict(X_test)
+    #     predicted_data = model.predict(X_test)
 
-        mlflow.sklearn.log_model(sk_model=model, artifact_path="model", input_example=input_example)
+    #     mlflow.sklearn.log_model(sk_model=model, artifact_path="model", input_example=input_example)
 
-        # Log Metrics
-        accuracy = model.score(X_test, y_test)
-        mlflow.log_metric("accuracy", accuracy)
+    #     # Log Metrics
+    #     accuracy = model.score(X_test, y_test)
+    #     mlflow.log_metric("accuracy", accuracy)
 
